60243010d81b09ac3d5c60d809f64bb7cc82b47d
Mais <59611735+itsmais@users.noreply.github.com>
2020-10-21 06:14:55 +0300

    fixed footer font

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,7 +66,7 @@ function addToYearsToArray() {
 function plotOrcas(year){
 
   document.getElementById("mapid").remove();
-  document.getElementById("map-container").innerHTML+=`<div id="mapid" style="width: 1000px; height: 500px;" ></div>`;
+  document.getElementById("map-container").innerHTML+=`<div id="mapid" style="width: 1000px; height: 400px;" ></div>`;
   mymap = L.map('mapid').setView([49,-123],7);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
   maxZoom: 18,
